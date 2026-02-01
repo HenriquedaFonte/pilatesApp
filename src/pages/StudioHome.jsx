30445cf8de2354e7c2e990cdf87f8dee44bf55f8
Henrique da Fonte <henriquegdafonte@gmail.com>
2026-02-01 10:00:38 -0500

    Fix testimonial ReferenceError by updating locale structure and code logic

--- src/pages/StudioHome.jsx
+++ src/pages/StudioHome.jsx
@@ -450,10 +450,11 @@ Sent from Josi Pilates website contact form.
             >
               <CarouselContent>
                 {testimonials.map((testimonial, index) => {
-                  const author =
-                    testimonial.city && testimonial.state
+                  const author = testimonial.author_name
+                    ? testimonial.city && testimonial.state
                       ? `${testimonial.author_name}, ${testimonial.city}, ${testimonial.state}`
                       : testimonial.author_name
+                    : testimonial.author
                   return (
                     <CarouselItem
                       key={testimonial.id || index}
@@ -487,34 +488,6 @@ Sent from Josi Pilates website contact form.
                     </CarouselItem>
                   )
                 })}
-                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                  <div className="p-1">
-                    <Card className="h-full">
-                      <CardContent className="flex flex-col justify-between p-6 h-full">
-                        <div>
-                          <div className="flex items-center mb-4">
-                            {[...Array(5)].map((_, i) => (
-                              <svg
-                                key={i}
-                                className="w-5 h-5 text-yellow-400 fill-current"
-                                viewBox="0 0 20 20"
-                              >
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                              </svg>
-                            ))}
-                          </div>
-                          <p className="text-gray-600 dark:text-gray-300 italic mb-4 flex-grow">
-                            "{testimonial.text}"
-                          </p>
-                        </div>
-                        <p className="text-sm font-semibold text-gray-800 dark:text-gray-200">
-                          - {testimonial.author}
-                        </p>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-                ) )}
               </CarouselContent>
               <div onClick={() => setAutoAdvancePaused(true)}>
                 <CarouselPrevious className="hidden md:flex" />
